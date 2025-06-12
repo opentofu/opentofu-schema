5db3dec21a9683332ce14e7078232d8ae779392c
Diogenes Fernandes <diofeher@gmail.com>
2025-06-12 15:11:32 -0300

    Testing with a map
    Signed-off-by: Diogenes Fernandes <diofeher@gmail.com>

--- internal/schema/0.12/resource_block.go
+++ internal/schema/0.12/resource_block.go
@@ -59,6 +59,10 @@ func resourceBlockSchema(v *version.Version) *schema.BlockSchema {
 						schema.Set{
 							Elem: schema.Reference{OfScopeId: refscope.ProviderScope},
 						},
+						schema.Map{
+							Name: "map of provider references",
+							Elem: schema.Reference{OfScopeId: refscope.ProviderScope},
+						},
 					},
 					IsOptional:             true,
 					Description:            lang.Markdown("Reference to a `provider` configuration block, e.g. `mycloud.west` or `mycloud`"),
