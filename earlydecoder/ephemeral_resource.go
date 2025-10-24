816c7cbc3fcd2b0726a683a8275090aca9043f1b
Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>
2025-10-24 14:19:08 +0400

    Copyright headers
    Signed-off-by: Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>

--- earlydecoder/ephemeral_resource.go
+++ earlydecoder/ephemeral_resource.go
@@ -1,3 +1,8 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) 2024 HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package earlydecoder
 
 import "fmt"
